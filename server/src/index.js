1b83fed0bef545669e9b359ec519438a3070445c
Afiya-830 <Afiyakittur2006@gmail.com>
2026-02-22 09:47:48 +0530

    "Day 6:Initializing Digital khata 's"

--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,31 @@ import express from "express";
 import route from "./routes/route.js";
 import jsonRoute from "./routes/jsonRoute.js"
 
-const apple = express();
+const app = express();
 
-apple.use("/", route);
-apple.use("/json",jsonRoute);
-apple.listen(5001,()=>{
+app.use("/", route);
+app.use("/json",jsonRoute);
+
+app.get('/owner', (req, res) =>{
+res.json({ owner: "Ramesh kaka" });
+});
+app.get('/customer/suresh',(req,res) => {
+    res.send('looking for customer suresh ');
+});
+
+app.get('/customers/:id', (req, res) => {
+const customerId = req.params.id;
+res.send(`Looking for Customer ID: ${customerId}`);
+});
+
+app.use(express.json());
+app.post('/customers', (req, res) => {
+    const newCustomer = req.body;
+    console.log('New Customer:', newCustomer);
+    res.status(201).json({ message: "customer saved!" , data: newCustomer });
+});
+
+
+app.listen(5001,()=>{
     console.log("🚀Counter Running at port 5001");
 });
